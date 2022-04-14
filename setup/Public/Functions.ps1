88198880bc804f7f0d0d1509ec9f13f652454b8c
Tobias Maestrini <tobias@bee365.ch>
2022-04-14 13:44:49 +0200

    improve exception handling

--- setup/Public/Functions.ps1
+++ setup/Public/Functions.ps1
@@ -41,7 +41,7 @@ function Add-ArtifactsToTenant() {
     Write-Output "> ADDING artifacts to app catalog on target tenant"
     $artifactsPath = Join-Path -Path $PSScriptRoot -ChildPath "../../assets/"
     $artifacts = @( Get-ChildItem -Path $artifactsPath\*.sppkg -ErrorAction SilentlyContinue )
-
+        
     foreach ($artifact in $artifacts) { 
         Write-Output "$($artifact)"
         Add-PnPApp -Path "$($artifact)" -Scope Tenant -Publish -Overwrite -SkipFeatureDeployment -Connection $Global:BcAdminConnection
@@ -52,15 +52,20 @@ function Grant-ServicePrincipalPermissionsInTenant() {
     $allowedPermissionRequestPackages = @("beeessentials-client-side-solution", "smartintranet-webparts-client-side-solution")
 
     Write-Output "> Grant specified permissions to the 'SharePoint Online Client Extensibility Web Application Principal' service principal for the webparts"
-    $requests = Get-PnPTenantServicePrincipalPermissionRequests -Connection $global:BcAdminConnection
-    foreach ($req in $requests) {
-        # $req
-        if ($allowedPermissionRequestPackages -match $req.PackageName) {
-            # Write-Host $req.PackageName
-            # Approve-PnPTenantServicePrincipalPermissionRequest -RequestId $req.Id.Guid -Connection $global:BcAdminConnection -Force
-            $req.Scope -split " " | % { Grant-PnPTenantServicePrincipalPermission -Scope $_ -Connection $global:BcAdminConnection }
-            Deny-PnPTenantServicePrincipalPermissionRequest -RequestId $req.Id.Guid -Connection $global:BcAdminConnection -Force
+    try {
+        $requests = Get-PnPTenantServicePrincipalPermissionRequests -Connection $global:BcAdminConnection
+        foreach ($req in $requests) {
+            # $req
+            if ($allowedPermissionRequestPackages -match $req.PackageName) {
+                # Write-Host $req.PackageName
+                # Approve-PnPTenantServicePrincipalPermissionRequest -RequestId $req.Id.Guid -Connection $global:BcAdminConnection -Force
+                $req.Scope -split " " | % { Grant-PnPTenantServicePrincipalPermission -Scope $_ -Connection $global:BcAdminConnection }
+                Deny-PnPTenantServicePrincipalPermissionRequest -RequestId $req.Id.Guid -Connection $global:BcAdminConnection -Force
+            }
         }
+    } catch {
+        Write-Output "❌ Error while trying to grant specified permissions for service principal. Please try running the script again!"
+        throw $_
     }
 }
 
@@ -69,7 +74,7 @@ function New-SmartIntranet() {
     Write-Output "> CREATING new beeConnect Site Collection: $($tenantConfig.SharePoint.PortalSite)"
     $siteUrl = "$($tenantConfig.SharePoint.Url)/$($tenantConfig.SharePoint.PortalSite)"
     New-PnPTenantSite -Url $siteUrl -Owner $tenantConfig.SharePoint.AdminUpn -Title $tenantConfig.SharePoint.PortalTitle -Template "SITEPAGEPUBLISHING#0" -Timezone 4 -LCID 1031 `
-                    -Wait -ErrorAction SilentlyContinue -Connection $global:BcAdminConnection
+                      -Wait -ErrorAction SilentlyContinue -Connection $global:BcAdminConnection
     $site = Get-PnPTenantSite -Identity $siteUrl -Connection $global:BcAdminConnection -ErrorAction $ErrorActionPreference
         
     # Set Org News Site
@@ -90,17 +95,18 @@ function New-SmartIntranet() {
         
     # Set Theme
     Write-Output "> ADJUSTING THEME AND WEB SETTINGS (lastly already done in PnPSiteTemplate)"
-    Set-PnPWeb -HeaderLayout "Extended" -HeaderEmphasis "Strong" -MembersCanShare:$false -CommentsOnSitePagesDisabled:$true -Connection $c
+    Set-PnPWeb -HeaderLayout Minimal -HeaderEmphasis "Strong" -MembersCanShare:$false -CommentsOnSitePagesDisabled:$true -Connection $c
 
     # Adjust Permissions
     Write-Output "> ADJUSTING PERMISSIONS"
     Set-StandardPermissions -Connection $c
         
-    Write-Output "> DONE! Don't forget to grant API access to the webparts through the SharePoint Admin Center..."
+    Write-Output "> ✅ DONE! Don't forget to grant API access to the webparts through the SharePoint Admin Center..."
 }
 
 function Set-StandardPermissions($Connection) {
     $ErrorAction = "Continue"
+    Write-Output "disregard any error in case of occurrence..."
     Set-PnPGroup -Identity (Get-PnPGroup -AssociatedOwnerGroup -Connection $Connection) -Title "SmartIntranet Admins" -OnlyAllowMembersViewMembership $true -Connection $Connection
     Set-PnPGroup -Identity (Get-PnPGroup -AssociatedMemberGroup -Connection $Connection) -Title "SmartIntranet ContentAdmins" -OnlyAllowMembersViewMembership $true -Connection $Connection
     Set-PnPGroup -Identity (Get-PnPGroup -AssociatedVisitorGroup -Connection $Connection) -Title "SmartIntranet Users" -OnlyAllowMembersViewMembership $true -Connection $Connection
